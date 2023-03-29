ffd70391ec8225cde8a839d93268fd08ca3751e5
PutonXD <129248036+PutonXD@users.noreply.github.com>
2023-03-29 12:50:11 +0700

    survey

--- Survey.php
+++ Survey.php
@@ -1,95 +1,82 @@
-<?php
-if (isset($_POST['submit'])) {
-    $kecepatan = $_POST['kecepatan'];
-    $ketepatan = $_POST['ketepatan'];
-    $keramahan = $_POST['keramahan'];
-    $kenyamanan = $_POST['kenyamanan'];
-    $total = $kecepatan + $ketepatan + $keramahan + $kenyamanan;
-    $rata = $total / 4;
-    if ($rata >= 4) {
-        $status = "Sangat Puas";
-    } elseif ($rata >= 3) {
-        $status = "Puas";
-    } elseif ($rata >= 2) {
-        $status = "Cukup Puas";
-    } else {
-        $status = "Tidak Puas";
-    }
-}
-?>
-
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Survey Kepuasan Pelanggan</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    </head>
+<html>
+<head>
+	<title>Survey Kepuasan Pelanggan</title>
+    <style>
+		label {
+			display: block;
+			margin-bottom: 5px;
+			font-weight: bold;
+			font-size: 14px;
+		}
+		input[type="number"] {
+			display: block;
+			width: 100%;
+			padding: 5px;
+			margin-bottom: 10px;
+			border: none;
+			border-bottom: 1px solid #ccc;
+			font-size: 16px;
+		}
+		input[type="submit"] {
+			display: block;
+			margin-top: 20px;
+			padding: 10px;
+			background-color: #4CAF50;
+			color: #fff;
+			border: none;
+			border-radius: 3px;
+			font-size: 16px;
+			cursor: pointer;
+		}
+	</style>
+</head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 class="text-center">Survey Kepuasan Pelanggan</h1>
-                <form action="" method="post">
-                    <div class="form-group">
-                        <label for="kecepatan">Kecepatan Pelayanan</label>
-                        <select name="kecepatan" id="kecepatan" class="form-control">
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="ketepatan">Ketepatan Pelayanan</label>
-                        <select name="ketepatan" id="ketepatan" class="form-control">
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="keramahan">Keramahan Pelayanan</label>
-                        <select name="keramahan" id="keramahan" class="form-control">
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="kenyamanan">Kenyamanan Pelayanan</label>
-                        <select name="kenyamanan" id="kenyamanan" class="form-control">
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" name="submit" value="Submit" class="btn btn-primary">
-                    </div>
-                </form>
-                <?php if (isset($_POST['submit'])) { ?>
-                    <div class="alert alert-success">
-                        <p>Kecepatan Pelayanan : <?= $kecepatan ?></p>
-                        <p>Ketepatan Pelayanan : <?= $ketepatan ?></p>
-                        <p>Keramahan Pelayanan : <?= $keramahan ?></p>
-                        <p>Kenyamanan Pelayanan : <?= $kenyamanan ?></p>
-                        <p>Total : <?= $total ?></p>
-                        <p>Rata-rata : <?= $rata ?></p>
-                        <p>Status : <?= $status ?></p>
-                    </div>
-                <?php } ?>
-            </div>
-        </div>
-    </div>
+	<h2>Survey Kepuasan Pelanggan</h2>
+    <h4>Silahkan masukkan angka 1-5, semakin banyak nilai yang anda berikan semakin tinggi tingkat kepuasan anda.</h4>
+	<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+		<label>Kecepatan Pelayanan : </label>
+		<input type="number" name="kecepatan" min="1" max="5"><br><br>
+		<label>Ketepatan Pelayanan : </label>
+		<input type="number" name="ketepatan" min="1" max="5"><br><br>
+		<label>Keramahan Pelayanan : </label>
+		<input type="number" name="keramahan" min="1" max="5"><br><br>
+		<label>Kenyamanan Pelayanan : </label>
+		<input type="number" name="kenyamanan" min="1" max="5"><br><br>
+		<input type="submit" name="submit" value="Submit">
+	</form>
+
+	<?php
+		if(isset($_POST['submit'])) {
+			$kecepatan = $_POST['kecepatan'];
+			$ketepatan = $_POST['ketepatan'];
+			$keramahan = $_POST['keramahan'];
+			$kenyamanan = $_POST['kenyamanan'];
+
+			$total = $kecepatan + $ketepatan + $keramahan + $kenyamanan;
+			$rata_rata = $total / 4;
+
+			echo "<h2>Hasil Survey</h2>";
+			echo "<p>Kecepatan Pelayanan : ".$kecepatan."</p>";
+			echo "<p>Ketepatan Pelayanan : ".$ketepatan."</p>";
+			echo "<p>Keramahan Pelayanan : ".$keramahan."</p>";
+			echo "<p>Kenyamanan Pelayanan : ".$kenyamanan."</p>";
+			echo "<p>Total Nilai : ".$total."</p>";
+			echo "<p>Rata-rata Nilai : ".$rata_rata."</p>";
+
+			if($rata_rata >= 4.5) {
+				echo "<p>Sangat Puas</p>";
+			}
+			elseif($rata_rata >= 3.5) {
+				echo "<p>Puas</p>";
+			}
+			elseif($rata_rata >= 2.5) {
+				echo "<p>Kurang</p>";
+			}
+			else {
+				echo "<p>Sangat Kurang</p>";
+			}
+		}
+	?>
 </body>
 </html>
